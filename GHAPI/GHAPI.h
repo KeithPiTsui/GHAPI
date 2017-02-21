2b3a07f04de0e4c70b609a7098a866d4f23e9aa0
KeithPiTsui <85331524@qq.com>
2017-02-21 19:29:46 +0800

    initial

---
+++ GHAPI/GHAPI.h
@@ -0,0 +1,19 @@
+//
+//  GHAPI.h
+//  GHAPI
+//
+//  Created by Pi on 21/02/2017.
+//  Copyright © 2017 Keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GHAPI.
+FOUNDATION_EXPORT double GHAPIVersionNumber;
+
+//! Project version string for GHAPI.
+FOUNDATION_EXPORT const unsigned char GHAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GHAPI/PublicHeader.h>
+
+
