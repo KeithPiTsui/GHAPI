e323953bd2fc42ee2f16c279e203365abf3687f8
KeithPiTsui <85331524@qq.com>
2017-02-25 16:03:38 +0800

    parse date complete

--- GHApi.playground/Contents.swift
+++ GHApi.playground/Contents.swift
@@ -11,18 +11,72 @@ import Result
 import ReactiveExtensions
 import GHAPI
 
-PlaygroundPage.current.needsIndefiniteExecution = true
+//PlaygroundPage.current.needsIndefiniteExecution = true
 
 //let str = "Hello world"
 
-let service = Service.init(
-    serverConfig: ServerConfig.github,
-    //oauthToken: OauthToken.init(token: "uncomment and put in your token!"),
-    language: "en"
-)
+//let service = Service.init(
+//    serverConfig: ServerConfig.github,
+//    //oauthToken: OauthToken.init(token: "uncomment and put in your token!"),
+//    language: "en"
+//)
+//
+//let x = service.testConnectionToGithub()
+//
+//x.startWithResult { (result) in
+//    print(result.value?.id ?? "No value")
+//}
 
-let x = service.testConnectionToGithub()
+//let dateString = "Thu, 22 Oct 2015 07:45:17 +0000"
+//let dateFormatter = NSDateFormatter()
+//dateFormatter.dateFormat = "EEE, dd MMM yyyy hh:mm:ss +zzzz"
+//dateFormatter.locale = Locale.init(identifier: "en_GB")
+//let dateObj = dateFormatter.dateFromString(dateString)
+//
+//dateFormatter.dateFormat = "MM-dd-yyyy"
+//print("Dateobj: \(dateFormatter.stringFromDate(dateObj!))")
 
-x.startWithResult { (result) in
-    print(result.value?.id ?? "No value")
-}
+//let dateString = "2015-05-12T01:01:22Z"
+  let dateString = "2017-02-23T13:48:53Z"
+let df = DateFormatter()
+/// 2015-05-12T01:01:22Z
+df.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+let date = df.date(from: dateString)
+
+//guard let date = df.date(from: dateString) else {
+////    return Decoded.failure(.custom("Date string misformatted"))
+//}
+
+
+//extension Date: Decodable {
+//    public static func decode(_ json: JSON) -> Decoded<Date> {
+//        switch json {
+//        case .string(let dateString):
+//            let df = DateFormatter()
+//            /// 2015-05-12T01:01:22Z
+//            df.dateFormat = "yyyy-MM-ddThh:mm:ssZ"
+//            guard let date = df.date(from: dateString) else {
+//                return Decoded.failure(.custom("Date string misformatted"))
+//            }
+//            return pure(date)
+//        default: return .typeMismatch(expected: "Date", actual: json)
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
