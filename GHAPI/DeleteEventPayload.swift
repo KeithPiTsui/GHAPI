ad41e92ea6bf7a791ba5738be4dcc0c7351eb2d3
KeithPiTsui <85331524@qq.com>
2017-03-17 16:20:24 +0800

    DeleteEventPayload

---
+++ GHAPI/DeleteEventPayload.swift
@@ -0,0 +1,54 @@
+//
+//  DeleteEventPayload.swift
+//  GHAPI
+//
+//  Created by Pi on 17/03/2017.
+//  Copyright © 2017 Keith. All rights reserved.
+//
+
+
+import Argo
+import Curry
+import Runes
+
+
+public struct DeleteEventPayload: EventPayloadType{
+  public static func decode(_ json: JSON) -> Decoded<EventPayloadType> {
+    return curry(DeleteEventPayload.init)
+      <^> json <| "ref"
+      <*> json <| "ref_type"
+    <*> json <| "pusher_type"
+    <*> json <| "repository"
+    <*> json <| "sender"
+  }
+
+  public func encode() -> [String : Any] {
+    var result: [String:Any] = [:]
+    result["ref"] = self.ref
+    result["ref_type"] = self.ref_type
+    result["pusher_type"] = self.pusher_type
+    result["repository"] = self.repository.encode()
+    result["sender"] = self.sender.encode()
+    return result
+  }
+
+  public let ref: String
+  public let ref_type: String
+  public let pusher_type: String
+  public let repository: Repository
+  public let sender: RepositoryOwner
+}
+
+extension DeleteEventPayload: GHAPIModelType {
+  public static func == (lhs: DeleteEventPayload, rhs: DeleteEventPayload) -> Bool {
+    return lhs.ref == rhs.ref
+    && lhs.ref_type == rhs.ref_type
+    && lhs.pusher_type == rhs.pusher_type
+    && lhs.repository == rhs.repository
+    && lhs.sender == rhs.sender
+  }
+
+  public var debugDescription: String {
+    return "DeleteEventPayload ref:\(self.ref)"
+  }
+}
